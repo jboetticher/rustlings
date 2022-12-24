287ff1c731ddd555cc66d5ec2baeae8f135b2154
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-12-23 21:43:59 -0500

    add clippy

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -1,8 +1,6 @@
 // macros2.rs
 // Execute `rustlings hint macros2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
